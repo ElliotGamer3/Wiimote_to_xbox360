aa1ce6b8f2d2cd842142198770ed3714f0f72d5a
ElliotImhoff <ElliotGamer3@gmail.com>
2019-05-06 16:35:21 -0500

    Changed How Accessory Mapping is Done
    ++ Wiimote Button to Keyboard Key Definitions
    ++ Wiimote Button to Mouse Key Definitions
    ++ Guitar Button to Keyboard Key Definitions
    ++ Nunchuck Button to Keyboard Key Definitions
    ++ Wiimote to Keyboard Mapping
    ++ Wiimote XOutput to Keyboard Mapping Toggle
    ++ More Specific Constants for Accessory Types
    ~~ Cleaned Up Player Updates
    TODO
    ~ Test New Accessory Mapping Method
    ~ Test Nunchuck Mapping
    + Guitar to Keyboard Mapping
    + Nunchuck to Keyboard Mapping
    + Wiimote Acceleration and Gyro

--- FreePIE Script/Wiimote_to_Xbox360.py
+++ FreePIE Script/Wiimote_to_Xbox360.py
@@ -37,43 +37,63 @@ MAX_PLAYERS = 4
 GUITAR_EXTENSION = 3
 NUNCHUCK_EXTENSION = 2
 NO_EXTENSION = 1
-OTHER_EXTENSION = -1
-
-#####################
-# Accessory updater #
-#####################
-
-player_type = list((0,0,0,0))
-
-def update_player_type():
-	for player in range(MAX_PLAYERS-1):
-		print(" Player: ", player, "\n")
-		
-		try:
-			wiimote[player].status.request()
-		except:
-			print(" Player ", player, " not connected\n")
-			continue
-			
-		if wiimote[player].capabilities.has_extension(WiimoteExtensions.None):
-			player_type[player] = NO_EXTENSION #None
-		elif wiimote[player].capabilities.has_extension(WiimoteExtensions.Nunchuck):
-			player_type[player] = NUNCHUCK_EXTENSION #Nunchuck
-		elif wiimote[player].capabilities.has_extension(WiimoteExtensions.GuitarHeroGuitar):
-			player_type[player] = GUITAR_EXTENSION #Guitar
-		else:
-			player_type[player] = OTHER_EXTENSION #disconnected
+UNSUPPORTED_EXTENSION = 0
+UNKNOWN_EXTENSION = -1
 
 ##############
 # Led Toggle #
 ##############
 
-def led_toggle(player_number, led_number):
-	wiimote[player_number].status.setLEDState(led_number, not(wiimote[player_number].status.getLEDState(led_number)))
+def toggle_LED(wiimote_num, led_number):
+	wiimote[wiimote_num].status.setLEDState(led_number, not(wiimote[wiimote_num].status.getLEDState(led_number)))
+
+##################################
+# Button to Mouse Button Binders #
+##################################
+
+def wButton_mButton(remote_num, wButton, mButton):
+	if wiimote[remote_num].buttons.button_down(wButton):
+		mouse.setButton(mButton, True)
+	else:
+		mouse.setButton(mButton, False)
+
+def gButton_mButton(remote_num, gButton, mButton):
+	if wiimote[remote_num].guitar.buttons.button_down(gButton):
+		mouse.setButton(mButton, True)
+	else:
+		mouse.setButton(mButton, False)
+		
+def nButton_mButton(remote_num, nButton, mButton):
+	if wiimote[remote_num].nunchuck.buttons.button_down(nButton):
+		mouse.setButton(mButton, True)
+	else:
+		mouse.setButton(mButton, False)
 
 #########################
-# Wiimote Button Binder #
+# Button to Key Binders #
 #########################
+
+def wButton_kButton(remote_num, wButton, kButton):
+	if wiimote[remote_num].buttons.button_down(wButton):
+		keyboard.setKeyDown(kButton)
+	else:
+		keyboard.setKeyUp(kButton)
+
+def gButton_kButton(remote_num, gButton, kButton):
+	if wiimote[remote_num].guitar.buttons.button_down(wButton):
+		keyboard.setKeyDown(kButton)
+	else:
+		keyboard.setKeyUp(kButton)
+      
+def nButton_kButton(remote_num, nButton, kButton):
+	if wiimote[remote_num].nunchuck.buttons.button_down(wButton):
+		keyboard.setKeyDown(kButton)
+	else:
+		keyboard.setKeyUp(kButton)
+
+##########################
+# Wiimote Button Binders #
+##########################
 
 def wButton_xButton(wiimote_num, xoutput_num, wButton, xButton):
 	xoutput[xoutput_num].setButton(xButton, wiimote[wiimote_num].buttons.button_down(wButton))
@@ -81,9 +101,9 @@ def wButton_xButton(wiimote_num, xoutput_num, wButton, xButton):
 def wButton_xButton(remote_num, wButton, xButton):
 	xoutput[remote_num].setButton(xButton, wiimote[remote_num].buttons.button_down(wButton))
 
-##########################
-# Nunchuck Button Binder #
-##########################
+###########################
+# Nunchuck Button Binders #
+###########################
 
 def nButton_xButton(wiimote_num, xoutput_num, nButton, xButton):
 	xoutput[xoutput_num].setButton(xButton, wiimote[wiimote_num].nunchuck.buttons.button_down(nButton))
@@ -91,9 +111,9 @@ def nButton_xButton(wiimote_num, xoutput_num, nButton, xButton):
 def nButton_xButton(remote_num, nButton, xButton):
 	xoutput[remote_num].setButton(xButton, wiimote[remote_num].nunchuck.buttons.button_down(nButton))
 
-########################
-# Guitar Button Binder #
-########################
+#########################
+# Guitar Button Binders #
+#########################
 
 def gButton_xButton(wiimote_num, xoutput_num, gButton, xButton):
 	xoutput[xoutput_num].setButton(xButton, wiimote[wiimote_num].guitar.buttons.button_down(gButton))
@@ -102,54 +122,71 @@ def gButton_xButton(remote_num, gButton, xButton):
 	xoutput[remote_num].setButton(xButton, wiimote[remote_num].guitar.buttons.button_down(gButton))
 
 ####################
-# Wiimote mappings #
+# Wiimote Mappings #
 ####################
 
-def map_wiimote(n):	
-	wButton_xButton(n, WiimoteButtons.A, XOutputButton.A)
-	wButton_xButton(n, WiimoteButtons.B, XOutputButton.B)
-	wButton_xButton(n, WiimoteButtons.One, XOutputButton.X)
-	wButton_xButton(n, WiimoteButtons.Two, XOutputButton.Y)
+diagnostics.watch(wiimote[0].buttons)
+
+def map_wiimote(wiimote_num):	
+	wButton_xButton(wiimote_num, WiimoteButtons.A, XOutputButton.A)
+	wButton_xButton(wiimote_num, WiimoteButtons.B, XOutputButton.B)
+	wButton_xButton(wiimote_num, WiimoteButtons.One, XOutputButton.X)
+	wButton_xButton(wiimote_num, WiimoteButtons.Two, XOutputButton.Y)
 	
-	wButton_xButton(n, WiimoteButtons.Plus, XOutputButton.Back)
-	wButton_xButton(n, WiimoteButtons.Minus, XOutputButton.Start)
+	wButton_xButton(wiimote_num, WiimoteButtons.Plus, XOutputButton.Back)
+	wButton_xButton(wiimote_num, WiimoteButtons.Minus, XOutputButton.Start)
 	
-	wButton_xButton(n, WiimoteButtons.DPadUp, XOutputButton.Up)
-	wButton_xButton(n, WiimoteButtons.DPadRight, XOutputButton.Right)
-	wButton_xButton(n, WiimoteButtons.DPadDown, XOutputButton.Down)
-	wButton_xButton(n, WiimoteButtons.DPadLeft, XOutputButton.Left)
+	wButton_xButton(wiimote_num, WiimoteButtons.DPadUp, XOutputButton.Up)
+	wButton_xButton(wiimote_num, WiimoteButtons.DPadRight, XOutputButton.Right)
+	wButton_xButton(wiimote_num, WiimoteButtons.DPadDown, XOutputButton.Down)
+	wButton_xButton(wiimote_num, WiimoteButtons.DPadLeft, XOutputButton.Left)
+	toggle_button_press(wiimote_num)
+	
+def map_wiimote_KB(wiimote_num):
+	wButton_kButton(wiimote_num, WiimoteButtons.A, Key.LeftShift)
+	wButton_kButton(wiimote_num, WiimoteButtons.B, Key.LeftAlt)
+	wButton_mButton(wiimote_num, WiimoteButtons.One, mouse.leftButton)
+	wButton_kButton(wiimote_num, WiimoteButtons.Two, Key.Space)
+	
+	wButton_kButton(wiimote_num, WiimoteButtons.Plus, Key.Escape)
+	wButton_kButton(wiimote_num, WiimoteButtons.Minus, Key.E)
+	
+	wButton_kButton(wiimote_num, WiimoteButtons.DPadUp, Key.A)
+	wButton_kButton(wiimote_num, WiimoteButtons.DPadRight, Key.W)
+	wButton_kButton(wiimote_num, WiimoteButtons.DPadDown, Key.D)
+	wButton_kButton(wiimote_num, WiimoteButtons.DPadLeft, Key.S)
+	toggle_button_press(wiimote_num)
 
 #####################
-# Nunchuck mappings #
+# Nunchuck Mappings #
 #####################
 
 #TODO::Test mapping for nunchuck stick
-def map_wiimote_nunchuck(n):
-	nButton_xButton(n, NunchuckButtons.C, XOutputButton.L1)
-	nButton_xButton(n, NunchuckButtons.Z, XOutputButton.R1)
-	xoutput[n].lx = wiimote[n].nunchuck.stick.x
-	xoutput[n].ly = wiimote[n].nunchuck.stick.y
+def map_wiimote_Nunchuck(remote_num):
+	nButton_xButton(remote_num, NunchuckButtons.C, XOutputButton.L1)
+	nButton_xButton(remote_num, NunchuckButtons.Z, XOutputButton.R1)
+	xoutput[remote_num].lx = wiimote[remote_num].nunchuck.stick.x
+	xoutput[remote_num].ly = wiimote[remote_num].nunchuck.stick.y
 
 ###################
-# Guitar mappings #
+# Guitar Mappings #
 ###################
 
-#TODO::Test mapping for guitars
-def map_wiimote_Guitar(n):
-	gButton_xButton(n, GuitarButtons.Green, XOutputButton.A)
-	gButton_xButton(n, GuitarButtons.Red, XOutputButton.B)
-	gButton_xButton(n, GuitarButtons.Yellow, XOutputButton.X)
-	gButton_xButton(n, GuitarButtons.Blue, XOutputButton.Y)
-	gButton_xButton(n, GuitarButtons.Orange, XOutputButton.R1)
+def map_wiimote_Guitar(wiimote_num):
+	gButton_xButton(wiimote_num, GuitarButtons.Green, XOutputButton.A)
+	gButton_xButton(wiimote_num, GuitarButtons.Red, XOutputButton.B)
+	gButton_xButton(wiimote_num, GuitarButtons.Yellow, XOutputButton.X)
+	gButton_xButton(wiimote_num, GuitarButtons.Blue, XOutputButton.Y)
+	gButton_xButton(wiimote_num, GuitarButtons.Orange, XOutputButton.R1)
 	
-	gButton_xButton(n, GuitarButtons.Minus, XOutputButton.Back)
-	gButton_xButton(n, GuitarButtons.Plus, XOutputButton.Start)
+	gButton_xButton(wiimote_num, GuitarButtons.Minus, XOutputButton.Back)
+	gButton_xButton(wiimote_num, GuitarButtons.Plus, XOutputButton.Start)
 	
-	gButton_xButton(n, GuitarButtons.StrumUp, XOutputButton.Up)
-	gButton_xButton(n, GuitarButtons.StrumDown, XOutputButton.Down)
+	gButton_xButton(wiimote_num, GuitarButtons.StrumUp, XOutputButton.Up)
+	gButton_xButton(wiimote_num, GuitarButtons.StrumDown, XOutputButton.Down)
 
 ###################
-# Wiimote updates #
+# Wiimote Updates #
 ###################
 
 def update_wiimote1():
@@ -163,25 +200,32 @@ def update_wiimote3():
 
 def update_wiimote4():
 	map_wiimote(3)
+	
+def update_wiimote1_KB():
+	map_wiimote_KB(0)
 
 ####################
-# Nunchuck updates #
+# Nunchuck Updates #
 ####################
 
-def update_nunchuck_wiimote1():
-	map_nunchuck_wiimote(0)
+def update_wiimote_Nunchuck1():
+	map_wiimote_Nunchuck(0)
 
-def update_nunchuck_wiimote2():
-	map_nunchuck_wiimote(1)
+def update_wiimote_Nunchuck2():
+	map_wiimote_Nunchuck(1)
 
-def update_nunchuck_wiimote3():
-	map_nunchuck_wiimote(2)
+def update_wiimote_Nunchuck3():
+	map_wiimote_Nunchuck(2)
 
-def update_nunchuck_wiimote4():
-	map_nunchuck_wiimote(3)
-
+def update_wiimote_Nunchuck4():
+	map_wiimote_Nunchuck(3)
+	
+#ADD MAPPING	
+def update_wiimote_Nunchuck1_KB():
+	return
+	
 ##################
-# Guitar updates #
+# Guitar Updates #
 ##################
 
 def update_wiimote_Guitar1():
@@ -196,81 +240,71 @@ def update_wiimote_Guitar3():
 def update_wiimote_Guitar4():
 	map_wiimote_Guitar(3)
 
+#ADD MAPPING
+def update_wiimote_Guitar1_KB():
+	return
+	
+##############################
+# Player One Keyboard Update #
+##############################
+
+def update_player_one_KB():
+	update_wiimote1_KB()
+	update_wiimote_Nunchuck1_KB()
+	update_wiimote_Guitar1_KB()
+
 ##################
-# Player updates #
+# Player Updates #
 ##################
 
 def update_player_one():
-	player_number = 0
-	if player_type[player_number] == OTHER_EXTENSION:
-		return False
-	elif player_type[player_number] == NO_EXTENSION:		
-		wiimote[player_number].buttons.update += update_wiimote1
-	elif player_type[player_number] == NUNCHUCK_EXTENSION:
-		wiimote[player_number].buttons.update += update_wiimote1
-		wiimote[player_number].nunchuck.update += update_nunchuck_wiimote1
-	elif player_type[player_number] == GUITAR_EXTENSION:
-		wiimote[player_number].guitar.update += update_wiimote_Guitar1
-	else:
-		print("Wiimote ", player_number, " not updated\n")
-		print("Unknown extension: ", player_type[player_number], "\n")
+	update_wiimote1()
+	update_wiimote_Nunchuck1()
+	update_wiimote_Guitar1()
 
 def update_player_two():
-	player_number = 1
-	if player_type[player_number] == OTHER_EXTENSION:
-		return False
-	elif player_type[player_number] == NO_EXTENSION:		
-		wiimote[player_number].buttons.update += update_wiimote2
-	elif player_type[player_number] == NUNCHUCK_EXTENSION:
-		wiimote[player_number].buttons.update += update_wiimote2
-		wiimote[player_number].nunchuck.update += update_nunchuck_wiimote2
-	elif player_type[player_number] == GUITAR_EXTENSION:
-		wiimote[player_number].guitar.update += update_wiimote_Guitar2
-	else:
-		print("Wiimote ", player_number, " not updated\n")
-		print("Unknown extension: ", player_type[player_number], "\n")
-
+	update_wiimote2()
+	update_wiimote_Nunchuck2()
+	update_wiimote_Guitar2()
+	
 def update_player_three():
-	player_number = 2
-	if player_type[player_number] == OTHER_EXTENSION:
-		return False
-	elif player_type[player_number] == NO_EXTENSION:		
-		wiimote[player_number].buttons.update += update_wiimote3
-	elif player_type[player_number] == NUNCHUCK_EXTENSION:
-		wiimote[player_number].buttons.update += update_wiimote3
-		wiimote[player_number].nunchuck.update += update_nunchuck_wiimote3
-	elif player_type[player_number] == GUITAR_EXTENSION:
-		wiimote[player_number].guitar.update += update_wiimote_Guitar3
-	else:
-		print("Wiimote ", player_number, " not updated\n")
-		print("Unknown extension: ", player_type[player_number], "\n")
-		
+	update_wiimote3()
+	update_wiimote_Nunchuck3()
+	update_wiimote_Guitar3()
+	
 def update_player_four():
-	player_number = 3
-	if player_type[player_number] == OTHER_EXTENSION:
-		return False
-	elif player_type[player_number] == NO_EXTENSION:		
-		wiimote[player_number].buttons.update += update_wiimote4
-	elif player_type[player_number] == NUNCHUCK_EXTENSION:
-		wiimote[player_number].buttons.update += update_wiimote4
-		wiimote[player_number].nunchuck.update += update_nunchuck_wiimote4
-	elif player_type[player_number] == GUITAR_EXTENSION:
-		wiimote[player_number].guitar.update += update_wiimote_Guitar4
-	else:
-		print("Wiimote ", player_number, " not updated\n")
-		print("Unknown extension: ", player_type[player_number], "\n")
+	update_wiimote4()
+	update_wiimote_Nunchuck4()
+	update_wiimote_Guitar4()
 
+########################
+# Capabilities Enabler #
+########################
+
+def enable_extensions(wiimote_num):
+	wiimote[wiimote_num].enable(WiimoteCapabilities.Extension)
+
+####################
+# Debug Definition #
+####################
+
+def toggle_button_press(wiimote_num):
+	if wiimote[wiimote_num].buttons.button_pressed(WiimoteButtons.Home):
+		toggle_LED(wiimote_num, wiimote_num)
+		
 ###############
 # Main Thingy #
 ###############
 
+if wiimote[0].status.getLEDState(0):
+	update_player_one_KB()
+else:
+	update_player_one()
+	
 if starting:
 	system.setThreadTiming(TimingTypes.HighresSystemTimer)
 	system.threadExecutionInterval = POLLING_PERIOD
-	
-	update_player_type()
-	
-	update_player_one()
+
 	update_player_two()
 	update_player_three()
 	update_player_four()
